5a5a99af75841924631e336cdab731e7a0e87641
Jan Gabriel <jan.gabriel@tul.cz>
2015-10-24 20:16:25 +0200

    Basic server test2

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
+  response.write("It's alive!!");
   response.end();
 }).listen(port);
