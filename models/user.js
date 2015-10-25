fe3f1b7cb73a37ea026db3e6b3388257f9d224a8
Jan Gabriel <jan.gabriel@tul.cz>
2015-10-25 12:35:33 +0100

    mongoose test

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: Date,
+  updated_at: Date
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
