9f37df4a3b9428996dee45ce21c61ed1f9ca475f
Jan Gabriel <jan.gabriel@tul.cz>
2015-11-24 20:50:31 +0100

    added test token

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -1,9 +1,16 @@
 'use strict'
 var Auth = require('../models/auth.js');
+var mongoose = require('mongoose');
+var User = require('../models/user.js');
 
 module.exports = {
     isTokenValid: function(req, res, next) {
         var token = req.headers["token"];
+        if (token == 'test') {
+            var id = mongoose.Types.ObjectId('5637286e9f35d76d2d6b05c0');
+            res.locals.me = User.findById(id);
+            return next();
+        }
         Auth
             .findOne()
             .where({
