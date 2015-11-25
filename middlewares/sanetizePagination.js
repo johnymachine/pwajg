e4633b42ba1546f18ae0ac2c5e3490c590b80f1c
Jan Gabriel <jan.gabriel@tul.cz>
2015-11-25 11:11:26 +0100

    sorting reversed

--- middlewares/sanetizePagination.js
+++ middlewares/sanetizePagination.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
-    sanetizePagination: function(req, res, next) { //order=asc; page=1; size=20
+    sanetizePagination: function(req, res, next) { //order=desc; page=1; size=20
         var validOrders = {
             desc: "desc",
             asc: "asc"
         };
-        res.locals.order = validOrders[req.query.order] || "asc";
+        res.locals.order = validOrders[req.query.order] || "desc";
         res.locals.page = Number(req.query.page) || 1;
         res.locals.size = Number(req.query.size) || 20;
         return next();
